556b062e196eea7c32810f45a65b02ab9a09fa0d
millie <amelia.g.crowther@gmail.com>
2018-06-02 10:30:12 +0100

    forgot to include the include folder yikes

---
+++ include/fusion.hpp
@@ -0,0 +1,32 @@
+#ifndef FUSION_H
+#define FUSION_H
+
+#include <string>
+#include <vector>
+#include "min_params.hpp"
+#include "cutil_math.hpp"
+
+class fusion_t {
+public:
+    // constructors and destructors
+    fusion_t(min_params_t * ps);
+    ~fusion_t();
+    
+    // main public method
+    void fusion();
+
+private:
+    // N.B. pointers are device pointers
+    int * device_phi;
+    float2 * phi_global;
+    float3 * psi;
+
+    // hyper parameters
+    min_params_t * ps;
+
+    // private methods
+    void get_sdf(int i, int * phi);
+    void update(bool is_rigid, int * phi);
+};
+
+#endif
---
+++ include/min_params.hpp
@@ -0,0 +1,15 @@
+#ifndef MIN_PARAMS_H
+#define MIN_PARAMS_H
+
+struct min_params_t {
+    int frames;
+
+    // image dimensions
+    int width;
+    int height;
+    int depth;
+
+    int voxel_length;
+};
+
+#endif
